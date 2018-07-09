2e0a34c2a3b7a4368b09ca162293e1f0c461998b
Taylor Donlan <taylordonlan@gmail.com>
2018-07-09 17:01:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,11 @@
 class Student
+  attr_accessor name:, grade:
+  attr_reader :id
 
+  def initialize(name, grade)
+    @name = name
+    @grade =  grade
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
