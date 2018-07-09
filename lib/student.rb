4759fc22bc98a57c6f26d7548c5b44a34da92688
Taylor Donlan <taylordonlan@gmail.com>
2018-07-09 17:07:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,15 @@ class Student
 
 
   def create_table
-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
