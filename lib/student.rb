53b23a2edaffc183ecc03268617721c30a7fb11d
Taylor Donlan <taylordonlan@gmail.com>
2018-07-09 17:05:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def self.create(name, grade)
+  def self.create(name:, grade:)
      student = Student.new(name, grade)
      student.save
      student
